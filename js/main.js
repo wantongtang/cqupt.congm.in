65a121d377691fc41994d4cab970040d5e62f537
mincong <i@congm.in>
2016-02-17 15:25:46 +0800

    链接地址转换

--- js/main.js
+++ js/main.js
@@ -57,3 +57,16 @@ var _hmt = _hmt || [];
         document.body.insertAdjacentHTML("afterbegin", a);
     }
 })();
+/* 链接地址转换 */
+(function(){
+    var all_a = document.getElementsByTagName("a");
+    var host_reg = /.+\.cqupt\.congm\.in/;
+    for(var i = 0; i < all_a.length; i++){
+        var url_array = all_a[i].href.split("/");
+        var protocol = url_array[0];
+        if(url_array.length > 2 && (protocol == "http:" || protocol == "https:") && !host_reg.text(url_array[2])){
+            url_array[2] += ".cqupt.congm.in";
+            all_a[i].href = url_array.join('/');
+        }
+    }
+})();
